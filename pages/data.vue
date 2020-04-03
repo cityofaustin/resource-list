a9b2710c3c62000a9ef10bceb6f2549be65ebc4d
Eric Sherman <ericandrewsherman@gmail.com>
2020-04-03 17:03:22 -0500

    ugly copy pasta ut works

--- pages/data.vue
+++ pages/data.vue
@@ -1,18 +1,216 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      {{ data }}
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      :page="page"
+      :search="search"
+      :sort-by="sortBy.toLowerCase()"
+      :sort-desc="sortDesc"
+      hide-default-footer
+    >
+      <template v-slot:header>
+        <v-toolbar
+          dark
+          color="blue darken-3"
+          class="mb-1"
+        >
+          <v-text-field
+            v-model="search"
+            clearable
+            flat
+            solo-inverted
+            hide-details
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+          />
+          <template v-if="$vuetify.breakpoint.mdAndUp">
+            <v-spacer />
+            <v-select
+              v-model="sortBy"
+              flat
+              solo-inverted
+              hide-details
+              :items="keys"
+              prepend-inner-icon="mdi-magnify"
+              label="Sort by"
+            />
+            <v-spacer />
+            <v-btn-toggle
+              v-model="sortDesc"
+              mandatory
+            >
+              <v-btn
+                large
+                depressed
+                color="blue"
+                :value="false"
+              >
+                <v-icon>mdi-arrow-up</v-icon>
+              </v-btn>
+              <v-btn
+                large
+                depressed
+                color="blue"
+                :value="true"
+              >
+                <v-icon>mdi-arrow-down</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </template>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="item in props.items"
+            :key="item.name"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-card-title class="subheading font-weight-bold">
+                {{ item.name }}
+              </v-card-title>
+
+              <v-divider />
+
+              <v-list dense>
+                <v-list-item
+                  v-for="(key, index) in filteredKeys"
+                  :key="index"
+                >
+                  <v-list-item-content
+                    :class="{ 'blue--text': sortBy === key }"
+                  >
+                    {{ key }}:
+                  </v-list-item-content>
+                  <v-list-item-content
+                    class="align-end"
+                    :class="{ 'blue--text': sortBy === key }"
+                  >
+                    {{ item[key.toLowerCase()] }}
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+
+      <template v-slot:footer>
+        <v-row
+          class="mt-2"
+          align="center"
+          justify="center"
+        >
+          <span class="grey--text">Items per page</span>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                dark
+                text
+                color="primary"
+                class="ml-2"
+                v-on="on"
+              >
+                {{ itemsPerPage }}
+                <v-icon>mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(number, index) in itemsPerPageArray"
+                :key="index"
+                @click="updateItemsPerPage(number)"
+              >
+                <v-list-item-title>{{ number }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
+          <v-spacer />
+
+          <span
+            class="mr-4
+            grey--text"
+          >
+            Page {{ page }} of {{ numberOfPages }}
+          </span>
+          <v-btn
+            fab
+            dark
+            color="blue darken-3"
+            class="mr-1"
+            @click="formerPage"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            color="blue darken-3"
+            class="ml-1"
+            @click="nextPage"
+          >
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-container>
 </template>
 <script>
-
-
 export default {
-// let data = (await $axios.get('https://data.austintexas.gov/resource/k4db-swbf.json`')).data
-async asyncData({ $axios }) {
-  const data = await $axios.$get('https://data.austintexas.gov/resource/k4db-swbf.json')
-  return { data }
-}
-}
+  // let data = (await $axios.get('https://data.austintexas.gov/resource/k4db-swbf.json`'))
+  async asyncData({ $axios }) {
+    const data = await $axios.$get(
+      "https://data.austintexas.gov/resource/k4db-swbf.json"
+    );
+
+    const keys = Object.keys(data[0]);
+    const headers = [];
+    for (const key in keys) {
+      headers.push(`{text: ${keys[key]}}`);
+    }
+    // TODO
+    // think about parsin into table vs data iterator, maybe check with design
+    // back up again and think about a simple list view
+    // focus on the data
+    debugger;
+    return {
+      itemsPerPageArray: [4, 8, 12],
+      search: "",
+      filter: {},
+      sortDesc: false,
+      page: 1,
+      itemsPerPage: 4,
+      sortBy: "name",
+      keys: keys,
+      items: data
+    };
+  },
+  computed: {
+    numberOfPages() {
+      return Math.ceil(this.items.length / this.itemsPerPage);
+    },
+    filteredKeys() {
+      return this.keys.filter(key => key !== `Name`);
+    }
+  },
+  methods: {
+    nextPage() {
+      if (this.page + 1 <= this.numberOfPages) this.page += 1;
+    },
+    formerPage() {
+      if (this.page - 1 >= 1) this.page -= 1;
+    },
+    updateItemsPerPage(number) {
+      this.itemsPerPage = number;
+    }
+  }
+};
 </script>
